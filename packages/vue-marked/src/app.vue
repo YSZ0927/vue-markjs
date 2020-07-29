155ead94cfd47908954ed1ce30be15d3b2232af0
shuizeng Yu <shuizengyu@91jkys.com>
2020-07-29 18:57:54 +0800

    init

---
+++ packages/vue-marked/src/app.vue
@@ -0,0 +1,35 @@
+<template>
+	<h1>Hello Vue 3!asdfasdf</h1>
+	<button @click="inc">Clicked {{ count }} times.</button>
+    <div v-html="markHtml"></div>
+</template>
+
+<script>
+	import { ref } from "vue";
+	import marked from "marked";
+	export default {
+		setup() {
+			console.log(marked);
+            const count = ref(0);
+            const markHtml = ref('')
+			const inc = () => {
+                count.value++;
+			    markHtml.value = marked("# Marked in the browser\n\nRendered by **marked**.");
+			};
+			return {
+				count,
+                inc,
+                markHtml,
+			};
+		},
+	};
+</script>
+
+<style scoped>
+	img {
+		width: 200px;
+	}
+	h1 {
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
---
+++ packages/vue-marked/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// @ts-ignore
+import App from './app.vue'
+
+createApp(App).mount('#app')
