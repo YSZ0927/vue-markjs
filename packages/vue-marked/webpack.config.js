042b1d4dffb4da361e4ff58f9e4591ddd191afe9
shuizeng Yu <shuizengyu@91jkys.com>
2021-06-27 09:32:31 +0800

    delete code

--- packages/vue-marked/webpack.config.js
+++ packages/vue-marked/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
@@ -11,10 +12,6 @@ module.exports = {
     },
     resolve: {
         alias: {
-            // this isn't technically needed, since the default `vue` entry for bundlers
-            // is a simple `export * from '@vue/runtime-dom`. However having this
-            // extra re-export somehow causes webpack to always invalidate the module
-            // on the first HMR update and causes the page to reload.
             'vue': '@vue/runtime-dom'
         }
     },
